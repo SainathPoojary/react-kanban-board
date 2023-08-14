1d425b722a1ad415f0d5b2b8767cd09b074c30ea
Sainath Poojary <spoojary614@gmail.com>
2023-08-14 19:59:24 +0530

    Added new icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import signal1 from "../assets/icons/signal-1.svg";
 import signal2 from "../assets/icons/signal-2.svg";
 import signal3 from "../assets/icons/signal-3.svg";
+import todoIcon from "../assets/icons/todo.svg";
+import inProgressIcon from "../assets/icons/inProgress.svg";
+import backlogIcon from "../assets/icons/backlog.svg";
 
 export default function Card({ ticket, group }) {
   function getSignalImage(priority) {
@@ -13,6 +16,19 @@ export default function Card({ ticket, group }) {
       return signal3;
     }
   }
+
+  function getStatusImage(status) {
+    if (status === "Todo") {
+      return todoIcon;
+    } else if (status === "Backlog") {
+      return backlogIcon;
+    } else if (status === "In progress") {
+      return inProgressIcon;
+    } else {
+      return todoIcon;
+    }
+  }
+
   return (
     <div className="flex flex-col px-5 py-3 bg-white shadow-md rounded-md  md:w-[26rem]">
       <div className="flex justify-between items-center">
@@ -28,15 +44,10 @@ export default function Card({ ticket, group }) {
           />
         )}
       </div>
-      <div className="flex gap-2 items-start mt-[5px] ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 text-gray-400"
-          viewBox="0 -960 960 960"
-          fill="currentColor"
-        >
-          <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
-        </svg>
+      <div className="flex gap-2 items-center mt-[5px] ">
+        {group !== "status" && (
+          <img src={getStatusImage(ticket.status)} className="h-6 w-6" alt="" />
+        )}
         <h3>{ticket.title}</h3>
       </div>
       <div className="flex items-center gap-2 mt-2">
